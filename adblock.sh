38115e287f6c4d375d75b2089f8e94c4430ec066
Pavel V <pavel@299792458.ru>
2020-12-05 21:01:50 +0300

    meh

--- adblock.sh
+++ adblock.sh
@@ -1,13 +1,12 @@
 set -euv
 
+wget -qc https://github.com/openwrt/packages/raw/master/net/adblock/files/adblock.sources
 declare -a adlists=$(cat adblock.sources | jq -c 'keys' | tr -d '"' | tr ',' ' ' | sed 's,^\[,,; s,\]$,,')
 
 [ -s 'dnsmasq-adblock.conf' ] && exit 1
 
 . adblock.list.sh
 
-i=0
-wget -qc https://github.com/openwrt/packages/raw/master/net/adblock/files/adblock.sources
 for ad in ${adlists[@]}; do
     #cat adblock.sources | jq ".$ad | (.url,.rule)" | sed '1d; $d; s/",$// ; s/^  "//' | while IFS=$'\n' read url; read rule;
     cat adblock.sources | jq ".$ad | (.url,.rule)" | sed 's/^"//; s/"$//' | while IFS=$'\n' read url; read rule;
@@ -20,7 +19,6 @@ for ad in ${adlists[@]}; do
             sed -e "s,^,address=/,; s,$,/," | pv -rab > dnsmasq-adblock-$ad.conf
         wc -l dnsmasq-adblock-$ad.conf
         echo
-        i=$(( $i + 1 ))
     done
 done
 
